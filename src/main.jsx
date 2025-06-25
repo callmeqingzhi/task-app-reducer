620015b065ed4374e10220f59252690824c7f804
chenzhao <798124928@qq.com>
2025-06-25 21:30:31 +0900

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
+
+// ✅ 设置全局带上 cookie
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
